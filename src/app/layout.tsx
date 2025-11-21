687a6acb24879eecc36bcccec03579fc8936ab1f
Nikos Kounakas <nikos.kounakas@diamir.io>
2025-11-22 00:11:05 +0100

    added tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,6 +14,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="de">
+      <head>
+        <script
+          defer
+          data-domain="192.168.0.10"
+          src="http://localhost:8081/js/script.js"
+        ></script>
+      </head>
       <body>{children}</body>
     </html>
   );
